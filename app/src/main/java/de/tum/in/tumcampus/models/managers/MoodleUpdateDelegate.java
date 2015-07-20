23054d91370b2e5fc427a22e4f392b7339ece6be
Philipp Fent <philipp@fent.de>
2015-07-20 11:25:02 +0200

    Extract moodle related code

--- app/src/main/java/de/tum/in/tumcampus/models/managers/MoodleUpdateDelegate.java
+++
@@ -1,8 +0,0 @@
-package de.tum.in.tumcampus.models.managers;
-
-/**
- * Delegate interface to implement the return calls of the MoodleManager
- */
-public interface MoodleUpdateDelegate {
-    void refresh();
-}
